5adb978335edd52abe9fb6308e424516a7b3117a
Michkay1814 <116459560+Michkay1814@users.noreply.github.com>
2023-05-05 23:31:27 -0400

    added numbers

---
+++ Expression.py
@@ -0,0 +1,13 @@
+# this program expresses all the mathematical operation
+num1 = int (input("enter the value of the first number"))
+num2 = int (input("enter the value of the second number"))
+# perform operation
+add =num1+num2
+sub =num1-num2
+div =num1/num2
+mult =num1*num2
+# bring result
+print("the sum of:" , num1, "and ", num2,"is", add )
+print("the sum of:" , num1, "and ", num2,"is", sub)
+print("the sum of:" , num1, "and ", num2,"is", div )
+print("the sum of:" , num1, "and ", num2,"is", mult )

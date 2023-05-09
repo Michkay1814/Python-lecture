663ec3af16a9921f7f6635b22e09a7ad46723b75
Michkay1814 <116459560+Michkay1814@users.noreply.github.com>
2023-05-08 21:49:55 -0400

    exercise

---
+++ exercise.py
@@ -0,0 +1,19 @@
+1.# Create a variable called `count` and set it to 10.
+2.# Use the `+=` assignment operator to add 5 to the `count` variable.
+3. #Print the value of `count` to the console.
+4.# Use the `-=` assignment operator to subtract 3 from the `count` variable.
+5.# Print the value of `count` to the console.
+6.# Use the `*=` assignment operator to multiply the `count` variable by 2.
+7.# Print the value of `count` to the console.
+
+count=10
+count+=5
+# add 5 to count
+print (count)
+#subtract 3 from "count"
+print(count)
+count-=3
+print (count) #print the value of 'count'
+# use the '*=' assignmentoperator to multiply the'count'variable by 2
+count*=2
+print(count)
---
+++ pythonoperator.py
@@ -0,0 +1,34 @@
+#python assignment operator
+w=10
+s=3
+print(w)
+s+=13 #s=s+13
+print(s)
+q=10
+q-=20 #q=q-20
+print(q)
+z=10
+z/=3
+print(z)
+p=32
+p%=7
+print(p)
+u=20
+u//=3
+print(u)
+
+#python comperison operator
+x=10
+y=4
+Python Exercise
+
+1.# Create a variable called `count` and set it to 10.
+2.# Use the `+=` assignment operator to add 5 to the `count` variable.
+3. #Print the value of `count` to the console.
+4.# Use the `-=` assignment operator to subtract 3 from the `count` variable.
+5.# Print the value of `count` to the console.
+6.# Use the `*=` assignment operator to multiply the `count` variable by 2.
+7.# Print the value of `count` to the console.
+
+count=10
+count +-5
